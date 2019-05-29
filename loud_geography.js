9af9e302d35ada944dc41b6862b1164a28d63e31
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-05-29 17:48:19 +0100

    loud geography

---
+++ loud_geography.js
@@ -0,0 +1,29 @@
+function setup() {
+  createCanvas(1200, 800);
+  noLoop();
+}
+
+function draw() {
+  background('lightcyan');
+  var step = 10;
+
+  for(var x = 0; x < 1200; x = x+step){
+    for(var y = 0; y < 800; y = y+step){
+      drawRandomLine(x,y,step);
+      // drawRandomShapes(x,y,step);
+    }
+  }
+}
+
+function drawRandomLine(x,y,step){
+  if(random() > 0.5) {
+    strokeWeight(step/2);
+    // stroke('burly');
+    line(x, y+step, x, y+step);
+
+  } else {
+    strokeWeight(step/4);
+    // stroke('mediumblue');
+    line(x+step, y, x+step, y)
+  }
+}
