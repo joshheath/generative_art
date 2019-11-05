700ffeeb87ccbf7384ef9c9c4c57d0015231af82
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-11-05 16:51:14 -0500

    add vertical oscillation

--- bevel_guide.js
+++ bevel_guide.js
@@ -1,9 +1,21 @@
+var createCanvas, background, width, height, ellipse, phase = 0, speed = 0.02, maxCircleSize = 20, sin, cos;
+
+
 function setup() {
-  createCanvas(500, 500);
+    "use strict";
+    createCanvas(500, 500);
 }
 
 function draw() {
-  background(90);
-    var x = width / 2, y = height / 2;
+    "use strict";
+
+    background(90);
+    var x = width / 2;
+    var y = height / 2 + sin(phase) * 50;
+    phase = frameCount * speed;
+    var sizeOffset = (cos(phase) + 1) * 0.5;
+    var circleSize = sizeOffset * maxCircleSize;
+
     ellipse(x, y, 15, 10);
+
 }
