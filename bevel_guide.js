590ea812bb9fdbcf9a60c2700fa6e3a539ef903e
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-11-05 17:23:36 -0500

    add columns for loop

--- bevel_guide.js
+++ bevel_guide.js
@@ -1,4 +1,4 @@
-var createCanvas, background, width, height, ellipse, phase = 0, speed = 0.03, maxCircleSize = 20, sin, cos;
+var createCanvas, background, width, height, ellipse, phase = 0, speed = 0.03, maxCircleSize = 20, sin, cos, frameCount, numRows = 10, sizeOffset, numCols = 10;
 
 
 function setup() {
@@ -13,9 +13,13 @@ function draw() {
     var x = width / 2;
     var y = height / 2 + sin(phase) * 50;
     phase = frameCount * speed;
-    var sizeOffset = (cos(phase) + 1) * 0.5;
     var circleSize = sizeOffset * maxCircleSize;
 
-    ellipse(x, y, circleSize, circleSize);
-
+    for (var col = 0; col < numCols; col +=1) {
+      for (var row = 0; row < numRows; row += 1) {
+        var y = height/2 + row * 10 + sin(phase) * 50;
+        var sizeOffset = (cos(phase - row / numRows) + 1) * 0.5;
+        ellipse(x, y, circleSize, circleSize);
+      }
+    }
 }
