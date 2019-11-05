417df3bc7bcc8e415c627d84d413851383a568f0
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-11-05 16:20:55 -0500

    add ellipse dimensions

--- bevel_guide.js
+++ bevel_guide.js
@@ -1,7 +1,9 @@
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
 }
 
 function draw() {
   background(90);
+    var x = width / 2, y = height / 2;
+    ellipse(x, y, 15, 10);
 }
