aeb80068b73dc8620c0b4da352ef7727a4dfdec9
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-11-06 11:57:49 -0500

    add colours and change background

--- bevel_guide.js
+++ bevel_guide.js
@@ -1,15 +1,18 @@
-var createCanvas, background, width, height, ellipse, phase = 0, speed = 0.03, maxCircleSize = 20, sin, cos, frameCount, numRows = 10, sizeOffset, numCols = 10;
+var createCanvas, background, width, height, ellipse, phase = 0, speed = 0.03, maxCircleSize = 20, sin, cos, frameCount, numRows = 10, sizeOffset, numCols = 10, colorA, colorB;
 
 
 function setup() {
     "use strict";
     createCanvas(500, 500);
+    noStroke();
+    colorA = color(253, 174, 120);
+    colorB = color(226, 129, 161);
 }
 
 function draw() {
     "use strict";
 
-    background(0);
+    background(4, 58, 74);
     var x = width / 2;
     var y = height / 2 + sin(phase) * 50;
     phase = frameCount * speed;
