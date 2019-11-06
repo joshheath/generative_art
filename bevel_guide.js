fdac5fe3d6ecd09a14ebf9b2f99b8ba7aef36e13
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-11-06 11:56:04 -0500

    small adjustment to sizeoffset var

--- bevel_guide.js
+++ bevel_guide.js
@@ -15,10 +15,12 @@ function draw() {
     phase = frameCount * speed;
     var circleSize = sizeOffset * maxCircleSize;
 
-    for (var col = 0; col < numCols; col +=1) {
+    for (var col = 0; col < numCols; col += 1) {
       for (var row = 0; row < numRows; row += 1) {
-        var y = height/2 + row * 10 + sin(phase) * 50;
-        var sizeOffset = (cos(phase - row / numRows) + 1) * 0.5;
+        var colOffset = map(col, 0, numCols, 0, TWO_PI);
+        var x = map(col, 0, numCols, 50, width - 50);
+        var y = height / 2 + row * 10 + sin(phase + colOffset) * 50;
+        var sizeOffset = (cos(phase - (row 0.1) + colOffset) + 1) * 0.5;
         ellipse(x, y, circleSize, circleSize);
       }
     }
