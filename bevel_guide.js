52d6f332ec245e5a37ee055df9a0c5116731c551
Josh Heath <37807868+joshheath@users.noreply.github.com>
2019-11-07 16:47:28 -0500

    tweaking

--- bevel_guide.js
+++ bevel_guide.js
@@ -1,5 +1,6 @@
-var createCanvas, background, width, height, ellipse, phase, speed, maxCircleSize, sin, cos, frameCount, numRows, sizeOffset, numCols, colorA, colorB, color, noStroke, numStrands;
+/* exported map */
 
+var createCanvas, background, width, height, ellipse, phase, speed, maxCircleSize, sin, cos, frameCount, numRows, numCols, colorA, colorB, color, noStroke, numStrands;
 
 function setup() {
     "use strict";
@@ -7,7 +8,7 @@ function setup() {
     // removes circle outlines
     noStroke();
 
-    phase = 0
+    phase = 0;
     // determines the speed each circle moves through a full iteration of size change from small to large to small
     speed = 0.03;
     // the largest diameter (in pixels) the circle with reach
@@ -27,27 +28,28 @@ function draw() {
     background(4, 58, 74);
     // frameCount counts frames over time, so it can be used to show change over time and therefore animation
     phase = frameCount * speed;
+    // iterating through the strands
     for (var strand = 0; strand < numStrands; strand +=1) {
-      var strandPhase = phase + map(strand, 0, numStrands, 0, TWO_PI);
-      // iterating through columns - along with the row loop below, this gives a grid structure
-      for (var col = 0; col < numCols; col += 1) {
-        // offsets the phase calculation based on column number
-        var colOffset = map(col, 0, numCols, 0, TWO_PI);
-        // x determines the position of the circles across the page
-        var x = map(col, 0, numCols, 50, width - 50);
+        var strandPhase = phase + map(strand, 0, numStrands, 0, TWO_PI);
+        // iterating through columns - along with the row loop below, this gives a grid structure
+        for (var col = 0; col < numCols; col += 1) {
+          // offsets the phase calculation based on column number
+            var colOffset = map(col, 0, numCols, 0, TWO_PI);
+            // x determines the position of the circles across the page
+            var x = map(col, 0, numCols, 50, width - 50);
 
-        for (var row = 0; row < numRows; row += 1) {
-          // y determines the position of circles up / down the page
-          var y = height / 2 + row * 10 + sin(strandPhase + colOffset) * 50;
-          // gives a cosine wave between 0 and 1 rather than -1 and 1
-          var sizeOffset = (cos(strandPhase - (row / numRows) + colOffset) + 1) * 0.5;
+            for (var row = 0; row < numRows; row += 1) {
+              // y determines the position of circles up / down the page
+              var y = height / 2 + row * 10 + sin(strandPhase + colOffset) * 50;
+              // gives a cosine wave between 0 and 1 rather than -1 and 1
+              var sizeOffset = (cos(strandPhase - (row / numRows) + colOffset) + 1) * 0.5;
 
-          // create a cosine of 20 moving between 0 and 1 (0-20 pixels)
-          var circleSize = sizeOffset * maxCircleSize;
-          // lerpColour blends two colours by an amount (variable 3)
-          fill(lerpColor(colorA, colorB, row / numRows));
-          ellipse(x, y, circleSize, circleSize);
+              // create a cosine of 20 moving between 0 and 1 (0-20 pixels)
+              var circleSize = sizeOffset * maxCircleSize;
+              // lerpColour blends two colours by an amount (variable 3)
+              fill(lerpColor(colorA, colorB, row / numRows));
+              ellipse(x, y, circleSize, circleSize);
+            }
         }
-      }
     }
 }
